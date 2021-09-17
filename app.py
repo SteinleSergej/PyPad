e0bbfb8ad9406f8e4ac39788aae067e538fe0eaf
Sergej Steinle <steinle.sergej.prog@gmail.com>
2021-09-17 21:00:49 +0200

    just finish.....

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import tkinter
+from tkinter.constants import TRUE
 
 from PIL import ImageTk,Image   #pip install pillow
-from tkinter import StringVar,IntVar, Widget,scrolledtext
+from tkinter import StringVar,IntVar, Widget,scrolledtext,messagebox,filedialog
+from tkinter import END
 #define window
 
 root = tkinter.Tk()
@@ -18,6 +20,7 @@ root_color = "#6c809a"
 root.config(bg=root_color)
 
 #define functions
+#dropdown need a event param
 def change_font(event):
     if options_var.get() == "normal":
         my_font = (font_family.get(),size_var.get())
@@ -26,6 +29,38 @@ def change_font(event):
 
     #change the font
     input_text.config(font=my_font)
+
+#make new note=>delete the input from scrolledtext widget [!]start is 1.0
+def new_note():
+    question = messagebox.askyesno("Are you sure?","Clear the workspace")
+    if question == TRUE:
+        input_text.delete("1.0",END)
+
+def save_note():
+    save_name = filedialog.asksaveasfilename(initialdir="./",title="Save Note",filetypes=(("Plain Text","*.txt"),("All Files","*.*")))
+    with open(save_name,'w') as f:
+        f.write(font_family.get()+ "\n")
+        f.write(str(size_var.get()) + "\n")
+        f.write(options_var.get() + "\n")
+        
+        f.write(input_text.get("1.0",END))
+
+def open_note():
+    
+    open_name = filedialog.askopenfilename(initialdir="./",title="Open Note",filetypes=(("Plain Text","*.txt"),("All Files","*.*")))
+
+    with open(open_name,'r') as f:
+        input_text.delete("1.0",END)
+        font_family.set(f.readline().strip())
+        size_var.set(int(f.readline().strip()))
+        options_var.set(f.readline().strip())
+
+        #I dont know why this function need a argument....(Marco: kamikaze häschen?)
+        change_font(1)
+        
+        text = f.read()
+        input_text.insert("1.0",text)
+        
 
 #---Define Layour--#
 
@@ -38,15 +73,15 @@ text_frame.pack(padx=5,pady=5)
 
 #Menu : new, open, save, font family, font size, font option
 new_image = ImageTk.PhotoImage(Image.open("icons/new.ico"))
-new_btn = tkinter.Button(menu_frame,image=new_image)
+new_btn = tkinter.Button(menu_frame,image=new_image,command=new_note)
 new_btn.grid(row=0,column=0,padx=5,pady=5)
 
 open_image = ImageTk.PhotoImage(Image.open("icons/open.ico"))
-open_btn = tkinter.Button(menu_frame,image=open_image)
+open_btn = tkinter.Button(menu_frame,image=open_image,command=open_note)
 open_btn.grid(row=0,column=1,padx=5,pady=5)
 
 save_image = ImageTk.PhotoImage(Image.open("icons/save.ico"))
-save_btn = tkinter.Button(menu_frame,image=save_image)
+save_btn = tkinter.Button(menu_frame,image=save_image,command=save_note)
 save_btn.grid(row=0,column=2,padx=5,pady=5)
 
 #list of fonts
